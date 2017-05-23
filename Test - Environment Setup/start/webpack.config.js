0cb6aef710e01622dc167d2af4dbab9292cf06fe
Anant <anantvijay.23@gmail.com>
2017-05-23 09:22:28 +0100

    Environment Setup

---
+++ Test - Environment Setup/start/webpack.config.js
@@ -0,0 +1,69 @@
+var webpack = require("webpack");
+var path = require('path');
+
+module.exports = {
+	entry: "./src/index.js",
+	output: {
+		path: path.resolve(__dirname, "dist/assets"),
+		filename: "bundle.js",
+		publicPath: "assets"
+	},
+	devServer: {
+		inline: true,
+		contentBase: './dist',
+		port: 3000
+	},
+	module: {
+		loaders: [
+			{
+				test: /\.js$/,
+				exclude: /(node_modules)/,
+				loader: 'babel-loader',
+				query: {
+					presets: ["latest", "stage-0", "react"]
+				}
+			},
+			{
+				test: /\.json$/,
+				exclude: /(node_modules)/,
+				loader: 'json-loader'
+			},
+			{
+				test: /\.css$/,
+				loader: 'style-loader!css-loader!autoprefixer-loader'
+			},
+			{
+				test: /\.scss$/,
+				loader: 'style-loader!css-loader!autoprefixer-loader!sass-loader'
+			}
+		]
+	}
+}
+
+// var webpack = require("webpack");
+
+// module.exports = {
+// 	entry: "./src/index.js",
+// 	output: {
+// 		path: "dist/assets",
+// 		filename: "bundle.js",
+// 		publicPath: "assets"
+// 	},
+// 	devServer: {
+// 		inline: true,
+// 		contentBase: './dist',
+// 		port: 3000
+// 	},
+// 	module: {
+// 		loaders: [
+// 			{
+// 				test: /\.js$/,
+// 				exclude: /(node_modules)/,
+// 				loader: ["babel-loader"],
+// 				query: {
+// 					presets: ["latest", "stage-0", "react"]
+// 				}
+// 			}
+// 		]
+// 	}
+// }
